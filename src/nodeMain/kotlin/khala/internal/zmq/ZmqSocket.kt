6e772aefcc82c1882bea2876537bce05bb4f37b4
ardenit <zaitsev.eo@gmail.com>
2020-09-09 02:01:32 +0300

    Learning zeromq.js API

--- src/nodeMain/kotlin/khala/internal/zmq/ZmqSocket.kt
+++ src/nodeMain/kotlin/khala/internal/zmq/ZmqSocket.kt
@@ -3,14 +3,41 @@ package khala.internal.zmq
 internal class ZmqSocket(val socket: dynamic) {
 
     fun connect(address: String) {
-        socket.connect(address)
+        if (isClosed) println("Socket is already closed!")
+        else socket.connect(address)
     }
 
+    var bindFinished = true
+    val bindQueue = ArrayDeque<String>()
+    var shouldClose = false
+    var isClosed = false
+
     fun bind(address: String) {
-        socket.bindSync(address)
+        if (isClosed) println("Socket is already closed!")
+        else if (!bindFinished) bindQueue.add(address)
+        else {
+            bindFinished = false
+            socket.bind(address) { err ->
+                bindFinished = true
+                if (err != undefined) {
+                    println(err)
+                }
+                if (bindQueue.isNotEmpty()) {
+                    bind(bindQueue.removeFirst())
+                }
+                else if (shouldClose) {
+                    close()
+                }
+            }
+        }
     }
 
     fun close() {
-        socket.close()
+        if (isClosed) println("Socket is already closed!")
+        else if (!bindFinished) shouldClose = true
+        else {
+            shouldClose = false
+            socket.close()
+        }
     }
 }
--- src/nodeTest/kotlin/khala/internal/zmq/ZmqMsgTest.kt
+++ src/nodeTest/kotlin/khala/internal/zmq/ZmqMsgTest.kt
@@ -3,8 +3,21 @@ package khala.internal.zmq
 import kotlin.test.Test
 
 class ZmqMsgTest {
+
     @Test
     fun test() {
         println("LOL")
+        val ctx = ZmqContext()
+        val sock1 = ctx.createDealerSocket()
+        val sock2 = ctx.createDealerSocket()
+        println("a")
+        sock1.bind("tcp://*:12345")
+        println("b")
+        sock2.connect("tcp://localhost:12345")
+        println("c")
+        sock2.close()
+        println("d")
+        sock1.close()
+        println("e")
     }
 }
