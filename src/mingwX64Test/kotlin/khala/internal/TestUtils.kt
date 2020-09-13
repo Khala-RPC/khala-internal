452660d144d91a9868883073e09fd9a58c424cb6
ardenit <zaitsev.eo@gmail.com>
2020-09-14 01:50:57 +0300

    Trying to fix strange build failure

--- src/mingwX64Test/kotlin/khala/internal/TestUtils.kt
+++ src/mingwX64Test/kotlin/khala/internal/TestUtils.kt
@@ -6,7 +6,7 @@ import platform.posix.sleep
 
 internal actual val supportedProtocols: List<String> = listOf("tcp", "inproc") // ipc uses UNIX domain sockets
 
-internal actual fun runTest(block: suspend () -> Unit) = runBlocking { block() }
+internal actual fun runTest(block: suspend () -> Unit): Unit = runBlocking { block() }
 
 internal actual fun checkForEvent(block: () -> Boolean, delay: Int, repeatCount: Int, finally: () -> Unit) {
     var repeats = 0
